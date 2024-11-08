934960d48e7d30200d8c01136d5d37894a8b910a
Infortru <160344951+Infortru@users.noreply.github.com>
2024-11-08 19:24:58 +0100

    Version 2.0 comentada

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let ventana1=document.querySelector("#ventana1");
 let ventana2=document.querySelector("#ventana2");
 let ventana3=document.querySelector("#ventana3");
 
+/*****Creación de las tabla de premios y displays*****/
 for (let j=0; j<4; j++){
     let div=document.createElement("div");
     div.classList.add("linea" + j);
@@ -37,36 +38,35 @@ ventana1.innerHTML=`<img src="img/rey_${incognito}.png">`;
 ventana2.innerHTML=`<img src="img/rey_${incognito}.png">`;
 ventana3.innerHTML=`<img src="img/rey_${incognito}.png">`;
 
+/*****Estas funciones realizan el juego de avances*****/
 function avance1(){
     
-    avances--;console.log(avances);
-    if(avances>=0){
+    avances--;
+    if(avances>=0){     //Comprueba que el número de avances es mayor que 0
         document.querySelector("#boton1").addEventListener("click", avance1);
         document.querySelector("#avances").innerHTML=`Avances ${avances}`;
         if(imagen1==6){
             imagen1=0    
             }else{
                 imagen1=imagen1+1;
-        }
-        ventana1.innerHTML=`<img src="img/rey_${imagenes[imagen1]}.png">`;
+        }       
+        ventana1.innerHTML=`<img src="img/rey_${imagenes[imagen1]}.png">`;  //Cambia la imagen de la ventana
         imagen2=imagen2; imagen3=imagen3;
     }
     premios();
 }
-   
-
 function avance2(){
 
     avances--;
-    if(avances>=0){
+    if(avances>=0){     //Comprueba que el número de avances es mayor que 0
         document.querySelector("#boton2").addEventListener("click", avance2);
         document.querySelector("#avances").innerHTML=`<div>Avances ${avances}</div>`;
         if(imagen2==6){
             imagen2=0 
         }else{
             imagen2=imagen2+1;
-        }
-        ventana2.innerHTML=`<img src="img/rey_${imagenes[imagen2]}.png">`;
+        }       
+        ventana2.innerHTML=`<img src="img/rey_${imagenes[imagen2]}.png">`;  //Cambia la imagen de la ventana
         imagen1=imagen1; imagen3=imagen3;
     }
     premios();
@@ -74,7 +74,7 @@ function avance2(){
 function avance3(){
 
     avances--;
-    if(avances>=0){
+    if(avances>=0){     //Comprueba que el número de avances es mayor que 0
         document.querySelector("#boton3").addEventListener("click", avance3);
         document.querySelector("#avances").innerHTML=`<div>Avances ${avances}</div>`;
         if(imagen3==6){
@@ -84,22 +84,25 @@ function avance3(){
             imagen3=imagen3+1;
         }
     }
-    ventana3.innerHTML=`<img src="img/rey_${imagenes[imagen3]}.png">`;
+    ventana3.innerHTML=`<img src="img/rey_${imagenes[imagen3]}.png">`;  //Cambia la imagen de la ventana
     imagen2=imagen2; imagen1=imagen1;
     premios();
 }
+
+/*****Función del juego principal*****/
 function jugar(){
     imagen1=Math.floor(Math.random()*imagenes.length);
     imagen2=Math.floor(Math.random()*imagenes.length);
     imagen3=Math.floor(Math.random()*imagenes.length);
-    
+    //Muestra las imágenes en las ventanas
     ventana1.innerHTML=`<img src="img/rey_${imagenes[imagen1]}.png">`;
     ventana2.innerHTML=`<img src="img/rey_${imagenes[imagen2]}.png">`;
     ventana3.innerHTML=`<img src="img/rey_${imagenes[imagen3]}.png">`;
-    
+    //Resta los créditos
     credito--;
-
+    //Llama a la función que gestiona los premios
     premios();
+    //Genera los avances
     let aleatorio=Math.floor(Math.random()*5);
     avances=Math.floor(Math.random()*5);
     if(aleatorio==avances){
@@ -107,6 +110,7 @@ function jugar(){
     } 
 }
 function premios(){
+    //Si las 3 imágenes son iguales ñade el premio correspondiente al crédito restante
     if(imagen1 == imagen2 && imagen2 == imagen3){
         switch (imagen1){
             case 0:premio=100
