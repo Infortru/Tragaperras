7369b18607eb62d72d106c934bd5b2acc5d6804f
Infortru <160344951+Infortru@users.noreply.github.com>
2024-11-08 19:02:33 +0100

    version2.0

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ let imagenes=["atanagildo", "ataulfo", "ervigio", "leogivildo", "recesvinto", "s
 let incognito="incognito";
 let credito=100;
 let premio=0;
+let avances=0;
+let imagen1, imagen2, imagen3;
+let ventana1=document.querySelector("#ventana1");
+let ventana2=document.querySelector("#ventana2");
+let ventana3=document.querySelector("#ventana3");
 
 for (let j=0; j<4; j++){
     let div=document.createElement("div");
@@ -25,16 +30,83 @@ for (let j=4; j<imagenes.length; j++){
 }
 document.querySelector("#tabla2").insertAdjacentHTML("beforeend",`<div id="credito">Crédito ${credito}</div>`);
 document.querySelector("#tabla2").insertAdjacentHTML("beforeend",`<div id="premio">Premio ${premio}</div>`);
+document.querySelector("#tabla2").insertAdjacentHTML("beforeend",`<div id="avances">Avances ${avances}</div>`);
 document.querySelector("#botonJugar").addEventListener("click", jugar);
 
-document.querySelector("#ventana1").innerHTML=`<img src="img/rey_${incognito}.png">`;
-document.querySelector("#ventana2").innerHTML=`<img src="img/rey_${incognito}.png">`;
-document.querySelector("#ventana3").innerHTML=`<img src="img/rey_${incognito}.png">`;
+ventana1.innerHTML=`<img src="img/rey_${incognito}.png">`;
+ventana2.innerHTML=`<img src="img/rey_${incognito}.png">`;
+ventana3.innerHTML=`<img src="img/rey_${incognito}.png">`;
 
+function avance1(){
+    
+    avances--;console.log(avances);
+    if(avances>=0){
+        document.querySelector("#boton1").addEventListener("click", avance1);
+        document.querySelector("#avances").innerHTML=`Avances ${avances}`;
+        if(imagen1==6){
+            imagen1=0    
+            }else{
+                imagen1=imagen1+1;
+        }
+        ventana1.innerHTML=`<img src="img/rey_${imagenes[imagen1]}.png">`;
+        imagen2=imagen2; imagen3=imagen3;
+    }
+    premios();
+}
+   
+
+function avance2(){
+
+    avances--;
+    if(avances>=0){
+        document.querySelector("#boton2").addEventListener("click", avance2);
+        document.querySelector("#avances").innerHTML=`<div>Avances ${avances}</div>`;
+        if(imagen2==6){
+            imagen2=0 
+        }else{
+            imagen2=imagen2+1;
+        }
+        ventana2.innerHTML=`<img src="img/rey_${imagenes[imagen2]}.png">`;
+        imagen1=imagen1; imagen3=imagen3;
+    }
+    premios();
+}
+function avance3(){
+
+    avances--;
+    if(avances>=0){
+        document.querySelector("#boton3").addEventListener("click", avance3);
+        document.querySelector("#avances").innerHTML=`<div>Avances ${avances}</div>`;
+        if(imagen3==6){
+            imagen3=0
+            
+        }else{
+            imagen3=imagen3+1;
+        }
+    }
+    ventana3.innerHTML=`<img src="img/rey_${imagenes[imagen3]}.png">`;
+    imagen2=imagen2; imagen1=imagen1;
+    premios();
+}
 function jugar(){
-    let imagen1=Math.floor(Math.random()*imagenes.length);
-    let imagen2=Math.floor(Math.random()*imagenes.length);
-    let imagen3=Math.floor(Math.random()*imagenes.length);
+    imagen1=Math.floor(Math.random()*imagenes.length);
+    imagen2=Math.floor(Math.random()*imagenes.length);
+    imagen3=Math.floor(Math.random()*imagenes.length);
+    
+    ventana1.innerHTML=`<img src="img/rey_${imagenes[imagen1]}.png">`;
+    ventana2.innerHTML=`<img src="img/rey_${imagenes[imagen2]}.png">`;
+    ventana3.innerHTML=`<img src="img/rey_${imagenes[imagen3]}.png">`;
+    
+    credito--;
+
+    premios();
+    let aleatorio=Math.floor(Math.random()*5);
+    avances=Math.floor(Math.random()*5);
+    if(aleatorio==avances){
+        avance1(); avance2(); avance3();
+    } 
+}
+function premios(){
     if(imagen1 == imagen2 && imagen2 == imagen3){
         switch (imagen1){
             case 0:premio=100
@@ -57,15 +129,10 @@ function jugar(){
     }else{
         premio=0;
     }
-    credito--;
-    
     mostrar(credito, premio);
-    
-    document.querySelector("#ventana1").innerHTML=`<img src="img/rey_${imagenes[imagen1]}.png">`;
-    document.querySelector("#ventana2").innerHTML=`<img src="img/rey_${imagenes[imagen2]}.png">`;
-    document.querySelector("#ventana3").innerHTML=`<img src="img/rey_${imagenes[imagen3]}.png">`;
 }
 function mostrar(credito, premio){
-    document.querySelector("#credito").innerHTML=`<div>Crédito ${credito}</div>`;
-    document.querySelector("#premio").innerHTML=`<div>Premio ${premio}</div>`;
+    document.querySelector("#credito").innerHTML=`Crédito ${credito}`;
+    document.querySelector("#premio").innerHTML=`Premio ${premio}`;
 }
+
